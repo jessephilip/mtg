f6c8a665aab36a811e4a1cb485e68946b0ae2af5
jessephilip <jessephilip@gmail.com>
2016-11-29 17:40:17 -0600

    getting further with oAuth

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -83,6 +83,16 @@ logo.on("click", function() {
 
 // ---------- FUNCTIONS ----------
 
+onSignIn();
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
 function search() {
     $.ajax({
             url: mtgQuery.urls.cardSearch + mtgQuery.parameters.name + searchTerm,
