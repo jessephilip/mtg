2125b17a27594208ad97b6665f1b424d8efd67a9
jessephilip <jessephilip@gmail.com>
2016-11-29 21:40:54 -0600

    troubleshooting presence

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -10,23 +10,33 @@ function logEntry(email) {
     });
 }
 
-// set up presence through firebase
+// ---------- PRESENCE THROUGH FIREBASE ----------
+// All of the connections will be stored in this directory.
+var connectionsRef = database.ref("/connections");
 
-var listRef = new Firebase("https://mtghelper-81c74.firebaseio.com/presence/");
-var userRef = listRef.push();
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
 
-// Add ourselves to presence list when online.
-var presenceRef = new Firebase("// https://mtghelper-81c74.firebaseio.com/.info/connected");
-presenceRef.on("value", function(snap) {
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
   if (snap.val()) {
-    // Remove ourselves when we disconnect.
-    userRef.onDisconnect().remove();
 
-    userRef.set(true);
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
   }
 });
 
-// Number of online users is the number of objects in the presence list.
-listRef.on("value", function(snap) {
-  console.log("# of online users = " + snap.numChildren());
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#watchers").html(snap.numChildren());
 });
