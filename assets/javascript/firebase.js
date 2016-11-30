cd864381dbc6e4e1014635438f4ef1b974cb3ffc
jessephilip <jessephilip@gmail.com>
2016-11-29 21:26:40 -0600

    trying to use firebase for presence

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,13 +1,32 @@
-// ---------- GLOBAL VARIABLES ----------
-
 // ---------- INITIATORS ----------
 var database = firebase.database();
 
 // ---------- ACTIONS ----------
 function logEntry(email) {
+    // firebase does not allow any directories to have a period in the string
     var emailFixed = email.replace(".", "");
-    console.log(emailFixed);
     database.ref(emailFixed + "/").push({
         time: "now"
     });
 }
+
+// set up presence through firebase
+
+var listRef = new Firebase("https://mtghelper-81c74.firebaseio.com/presence/");
+var userRef = listRef.push();
+
+// Add ourselves to presence list when online.
+var presenceRef = new Firebase("// https://mtghelper-81c74.firebaseio.com/.info/connected");
+presenceRef.on("value", function(snap) {
+  if (snap.val()) {
+    // Remove ourselves when we disconnect.
+    userRef.onDisconnect().remove();
+
+    userRef.set(true);
+  }
+});
+
+// Number of online users is the number of objects in the presence list.
+listRef.on("value", function(snap) {
+  console.log("# of online users = " + snap.numChildren());
+});
