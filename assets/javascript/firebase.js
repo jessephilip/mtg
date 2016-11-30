e96048cefce855141f69119c074ba09a2aaf9a5f
jessephilip <jessephilip@gmail.com>
2016-11-29 21:17:11 -0600

    need to replace any periods in an email

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -5,7 +5,9 @@ var database = firebase.database();
 
 // ---------- ACTIONS ----------
 function logEntry(email) {
-    database.ref(email + "/").push({
+    var emailFixed = email.replace(".", "");
+    console.log(emailFixed);
+    database.ref(emailFixed + "/").push({
         time: "now"
     });
 }
